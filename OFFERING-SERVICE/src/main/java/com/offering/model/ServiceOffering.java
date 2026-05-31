72b0af01ed872e59fa4f6ec398bd22a4599645e4
GovindaEkbote <govindaekbote7@gmail.com>
2026-06-01 01:38:48 +0530

    create service Offer service

---
+++ OFFERING-SERVICE/src/main/java/com/offering/model/ServiceOffering.java
@@ -0,0 +1,30 @@
+package com.offering.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class ServiceOffering {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private  String name;
+    @Column(nullable = false)
+    private  String description;
+    @Column(nullable = false)
+    private int price;
+    @Column(nullable = false)
+    private int duration;
+    @Column(nullable = false)
+    private Long salonId;
+    @Column(nullable = false)
+    private Long categoryId;
+
+    private String images;
+
+
+}
---
+++ OFFERING-SERVICE/src/main/java/com/offering/service/ServiceOfferingService.java
@@ -0,0 +1,20 @@
+package com.offering.service;
+
+import com.offering.dto.CategoryDTO;
+import com.offering.dto.SaloneDTO;
+import com.offering.dto.ServiceDTO;
+import com.offering.model.ServiceOffering;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public interface ServiceOfferingService {
+
+    ServiceOffering saveServiceOffering(SaloneDTO saloneDTO, CategoryDTO categoryDTO, ServiceDTO serviceDTO);
+    ServiceOffering updateService(Long serviceId, ServiceOffering service);
+    Set<ServiceOffering> getAllServiceBySalonId(Long salonId, Long categoryId);
+    Set<ServiceOffering> getServicesByIds(Set<Long> ids);
+
+}
